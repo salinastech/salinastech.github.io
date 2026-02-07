4d5461156c0039786f1ba8f484380a3bf4310391
SALINAS TECH <salinastechbolivia@gmail.com>
2026-02-07 12:41:41 -0400

    Update script.js

--- recurso/visualizador/script.js
+++ recurso/visualizador/script.js
@@ -3,7 +3,6 @@ const visualizador = document.getElementById('visualizador');
 const errorMsg = document.getElementById('error-msg');
 const rgbText = document.getElementById('rgbText');
 
-// Funciona al presionar Enter
 hexInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') verColor();
 });
@@ -11,24 +10,21 @@ hexInput.addEventListener('keypress', (e) => {
 function verColor() {
     let hex = hexInput.value.trim();
     
-    // Auto-completar '#'
     if (hex.length > 0 && !hex.startsWith('#')) {
         hex = '#' + hex;
         hexInput.value = hex;
     }
     
-    // Validación Hexadecimal
     const regex = /^#([A-Fa-f0-9]{3}){1,2}$/;
     
     if (regex.test(hex)) {
         errorMsg.style.display = 'none';
         visualizador.style.backgroundColor = hex;
-        
-        // Conversión a RGB (Enteros)
+    
         const rgb = hexToRgb(hex);
         rgbText.innerText = `RGB: (${rgb.r}, ${rgb.g}, ${rgb.b})`;
         
-        // Ajustar borde si es muy claro
+        //
         const brillo = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
         visualizador.style.borderColor = brillo > 230 ? '#eee' : '#fff';
     } else {
@@ -46,4 +42,5 @@ function hexToRgb(hex) {
     const b = parseInt(h.slice(5, 7), 16);
     
     return { r: Math.round(r), g: Math.round(g), b: Math.round(b) };
+
 }
